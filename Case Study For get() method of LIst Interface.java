1d437defe21839bb471ab8093c033ed3a7065933
Rajat Rawat <rajatrawat1997@gmail.com>
2018-03-15 20:46:34 +0530

    Add files via upload

---
+++ Case Study For get() method of LIst Interface.java
@@ -0,0 +1,19 @@
+package CF;
+
+import java.util.*;
+
+class Main {
+    
+	public static void main(String[] afrbs)
+	{
+		List l=new ArrayList(5);
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		l.add(4);
+		l.add(5);
+		l.get(-1);
+		System.out.println(l);
+	}
+
+}
